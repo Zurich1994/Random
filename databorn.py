c00693a11d58eed30b5b90e7d7e1f350a954e615
Zurich1994 <huanran.wang1994@foxmail.com>
2018-10-24 21:10:53 +0800

    Initial commit

---
+++ databorn.py
@@ -0,0 +1,107 @@
+# coding = utf-8
+import random
+
+base_num1 = [76.43, 76.31, 76.38]
+print("第一组：\n")
+for i in range(1,6):
+	print("%.2f " % random.uniform(base_num1[0] - 2.00, base_num1[0] + 2.00), end="")
+	print("%.2f " % random.uniform(base_num1[1]-i - 1.00, base_num1[1]-i + 1.00), end="")
+	print("%.2f " % random.uniform(base_num1[2]-i - 1.00, base_num1[2]-i + 1.00))
+
+base_num2 = [63.83, 63.82, 63.83]
+print("\n第二组：\n")
+for i in range(1,6):
+	print("%.2f " % random.uniform(base_num2[0] - 2.00, base_num2[0] + 2.00), end="")
+	print("%.2f " % random.uniform(base_num2[1]-i - 1.00, base_num2[1]-i + 1.00), end="")
+	print("%.2f " % random.uniform(base_num2[2]-i - 1.00, base_num2[2]-i + 1.00))
+
+base_num3 = [72.67, 72.68, 72.64]
+print("\n第三组：\n")
+for i in range(1,6):
+	print("%.2f " % random.uniform(base_num3[0] - 2.00, base_num3[0] + 2.00), end="")
+	print("%.2f " % random.uniform(base_num3[1]-i - 1.00, base_num3[1]-i + 1.00), end="")
+	print("%.2f " % random.uniform(base_num3[2]-i - 1.00, base_num3[2]-i + 1.00))
+
+base_num4 = [73.83, 73.85, 73.83]
+print("\n第四组：\n")
+for i in range(1,6):
+	print("%.2f " % random.uniform(base_num4[0] - 2.00, base_num4[0] + 2.00), end="")
+	print("%.2f " % random.uniform(base_num4[1]-i - 1.00, base_num4[1]-i + 1.00), end="")
+	print("%.2f " % random.uniform(base_num4[2]-i - 1.00, base_num4[2]-i + 1.00))
+
+base_num5 = [64.73, 64.75, 64.74]
+print("\n第五组：\n")
+for i in range(1,6):
+	print("%.2f " % random.uniform(base_num5[0] - 2.00, base_num5[0] + 2.00), end="")
+	print("%.2f " % random.uniform(base_num5[1]-i - 1.00, base_num5[1]-i + 1.00), end="")
+	print("%.2f " % random.uniform(base_num5[2]-i - 1.00, base_num5[2]-i + 1.00))
+
+base_num6 = [96.79, 95.5, 94.78]
+print("\n第六组：\n")
+for i in range(1,6):
+	print("%.2f " % random.uniform(base_num6[0] - 2.00, base_num6[0] + 2.00), end="")
+	print("%.2f " % random.uniform(base_num6[1]-i - 1.00, base_num6[1]-i + 1.00), end="")
+	print("%.2f " % random.uniform(base_num6[2]-i - 1.00, base_num6[2]-i + 1.00))
+
+base_num7 = [89.03, 90.21, 94.78]
+print("\n第七组：\n")
+for i in range(1,6):
+	print("%.2f " % random.uniform(base_num7[0] - 2.00, base_num7[0] + 2.00), end="")
+	print("%.2f " % random.uniform(base_num7[1]-i - 1.00, base_num7[1]-i + 1.00), end="")
+	print("%.2f " % random.uniform(base_num7[2]-i - 1.00, base_num7[2]-i + 1.00))
+
+base_num8 = [79.20, 80.32, 79.43]
+print("\n第八组：\n")
+for i in range(1,6):
+	print("%.2f " % random.uniform(base_num8[0] - 2.00, base_num8[0] + 2.00), end="")
+	print("%.2f " % random.uniform(base_num8[1]-i - 1.00, base_num8[1]-i + 1.00), end="")
+	print("%.2f " % random.uniform(base_num8[2]-i - 1.00, base_num8[2]-i + 1.00))
+
+base_num9 = [93.12, 93.04, 92.70]
+print("\n第九组：\n")
+for i in range(1,6):
+	print("%.2f " % random.uniform(base_num9[0] - 2.00, base_num9[0] + 2.00), end="")
+	print("%.2f " % random.uniform(base_num9[1]-i - 1.00, base_num9[1]-i + 1.00), end="")
+	print("%.2f " % random.uniform(base_num9[2]-i - 1.00, base_num9[2]-i + 1.00))
+
+base_num10 = [83.12, 84.70, 83.03]
+print("\n第十组：\n")
+for i in range(1,6):
+	print("%.2f " % random.uniform(base_num10[0] - 2.00, base_num10[0] + 2.00), end="")
+	print("%.2f " % random.uniform(base_num10[1]-i - 1.00, base_num10[1]-i + 1.00), end="")
+	print("%.2f " % random.uniform(base_num10[2]-i - 1.00, base_num10[2]-i + 1.00))
+
+base_num11 = [94.79, 95.02, 94.30]
+print("\n第十一组：\n")
+for i in range(1,6):
+	print("%.2f " % random.uniform(base_num11[0] - 2.00, base_num11[0] + 2.00), end="")
+	print("%.2f " % random.uniform(base_num11[1]-i - 1.00, base_num11[1]-i + 1.00), end="")
+	print("%.2f " % random.uniform(base_num11[2]-i - 1.00, base_num11[2]-i + 1.00))
+
+base_num12 = [63.83, 63.82, 63.83]
+print("\n第十二组：\n")
+for i in range(1,6):
+	print("%.2f " % random.uniform(base_num12[0] - 2.00, base_num12[0] + 2.00), end="")
+	print("%.2f " % random.uniform(base_num12[1]-i - 1.00, base_num12[1]-i + 1.00), end="")
+	print("%.2f " % random.uniform(base_num12[2]-i - 1.00, base_num12[2]-i + 1.00))
+
+base_num13 = [72.67, 72.68, 72.64]
+print("\n第十三组：\n")
+for i in range(1,6):
+	print("%.2f " % random.uniform(base_num13[0] - 2.00, base_num13[0] + 2.00), end="")
+	print("%.2f " % random.uniform(base_num13[1]-i - 1.00, base_num13[1]-i + 1.00), end="")
+	print("%.2f " % random.uniform(base_num13[2]-i - 1.00, base_num13[2]-i + 1.00))
+
+base_num14 = [73.83, 73.85, 73.83]
+print("\n第十四组：\n")
+for i in range(1,6):
+	print("%.2f " % random.uniform(base_num14[0] - 2.00, base_num14[0] + 2.00), end="")
+	print("%.2f " % random.uniform(base_num14[1]-i - 1.00, base_num14[1]-i + 1.00), end="")
+	print("%.2f " % random.uniform(base_num14[2]-i - 1.00, base_num14[2]-i + 1.00))
+
+base_num15 = [64.73, 64.75, 64.74]
+print("\n第十五组：\n")
+for i in range(1,6):
+	print("%.2f " % random.uniform(base_num15[0] - 2.00, base_num15[0] + 2.00), end="")
+	print("%.2f " % random.uniform(base_num15[1]-i - 1.00, base_num15[1]-i + 1.00), end="")
+	print("%.2f " % random.uniform(base_num15[2]-i - 1.00, base_num15[2]-i + 1.00))
